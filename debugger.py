d9eef82a488263b032f419fab8de8e915b5c1248
Vipul-crossml <vipul@crossml.com>
2021-08-07 16:45:25 +0530

    session3 debugger

---
+++ debugger.py
@@ -0,0 +1,13 @@
+import pdb
+
+
+# make a simple function to debugg
+def func(n):
+    for i in range(6):
+        print("Hello! ", i + 1)
+
+
+# starting point to debugg
+pdb.set_trace()
+func(10)
+fu(4)
