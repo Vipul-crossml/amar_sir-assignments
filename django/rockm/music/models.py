46a29a4364307f88bb14ca84b0931faa10c2c1bd
Vipul-crossml <vipul@crossml.com>
2021-08-15 23:32:32 +0530

    my todo project

--- django/rockm/music/models.py
+++ django/rockm/music/models.py
@@ -4,12 +4,13 @@ from django.db import models
 class Singer(models.Model):
     singer_name = models.CharField(max_length=200)
 
+class Song(models.Model):
+    album_name = models.ForeignKey(Album,on_delete=models.CASCADE)
+    song_name = models.CharField(max_length=200)
+    song_length = models.IntegerField(default=0)
+    
 class Album(models.Model):
     singer_name = models.ManyToManyField(Singer)
     album_name = models.CharField(max_length=200)
     album_lang = models.CharField(max_length=200)
     no_of_songs = models.IntegerField(default=0)
-class Song(models.Model):
-    album_name = models.ForeignKey(Album,on_delete=models.CASCADE)
-    song_name = models.CharField(max_length=200)
-    song_length = models.IntegerField(default=0)
