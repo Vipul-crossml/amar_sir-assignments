db8cbf49a13b88ec24292a600f10fbaca76a8b60
yakul-crossml <88380853+yakul-crossml@users.noreply.github.com>
2021-08-20 14:31:07 +0530

    review code

--- weekly assignment 1/Weekly_assignment_1.py
+++ weekly assignment 1/Weekly_assignment_1.py
@@ -4,6 +4,9 @@ import os
 import getpass
 import uuid
 import json
+							# make a class that perform all basic operation like generate id and email and display usernames and for store the data in json file you can greate seprate function
+
+							# you can make a one function to display time and username
 
 # current datetime
 now = datetime.datetime.now()
@@ -12,15 +15,16 @@ current_date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 print(current_date_time)
 print(getpass.getuser())
 
+
 PWD = os.getcwd()
 print(PWD)
-users_folder = PWD+'/users/'
+users_folder = PWD+'/users/'			#we can add the file name dynamically by input 
 
 files_list = os.listdir(users_folder)
 person_names_list = []
 
 def display_names(users_folder):
-	files_list = os.listdir(users_folder)
+	files_list = os.listdir(users_folder)		
 	for file_name in files_list:
 		# print(file_name)
 		# file = open(users_folder+file_name,'r')
@@ -34,7 +38,7 @@ person_names = [user.strip() for user in person_names_list]
 def gen_UUID(user):
 	return user[0]+str(uuid.uuid1())
 
-def gen_email(person_name):
+def gen_email(person_name):				# this function is not valid if two users have same name than the email become same for both user for this you can use auto increment method for each email
 	first_name = person_name[0:4]
 	last_name = person_name[-1]
 	return first_name+last_name+"@try.com"
